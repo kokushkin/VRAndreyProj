5128bdb319c16f852842ce653f890ddb70dde7cd
Nikolai Kulikov <nikolai.kulikov.dev@gmail.com>
2021-09-10 00:35:52 +0300

    end of the 4th chapter

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    public void SetColourYellow()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.yellow;
+        
+    }
+
+    public void SetColourGreen()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.green;
+
+    }
+}
