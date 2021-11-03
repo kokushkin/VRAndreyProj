56eaf4e3fa68b535f3c523c4fa749909f42acf5f
Nikolai Kulikov <nikolai.kulikov.dev@gmail.com>
2021-11-03 23:13:17 +0200

    Lesson 05 - add weapons

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Weapon : MonoBehaviour
+{
+    private XRGrabInteractable interactable = null;
+
+    private void Awake()
+    {
+        interactable = GetComponent<XRGrabInteractable>();
+
+    }
+
+    private void OnEnable()
+    {
+        interactable.onActivate.AddListener(Fire);
+    }
+
+
+    private void OnDisable()
+    {
+        interactable.onActivate.RemoveListener(Fire);
+
+    }
+
+    private void Fire(XRBaseInteractor interactable)
+    {
+        print("Fire");
+
+    }
+}
