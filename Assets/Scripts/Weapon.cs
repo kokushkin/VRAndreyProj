624dce25a79cf927cde553d4f154da50c0710921
Nikolai Kulikov <nikolai.kulikov.dev@gmail.com>
2021-11-07 13:01:43 +0200

    Lesson 07 - make a target

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,10 +6,12 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Weapon : MonoBehaviour
 {
+    public float recoil = 1.0f;
     public Transform barrel = null;
     public GameObject projectilePrefab = null;
 
     private XRGrabInteractable interactable = null;
+    private Rigidbody rbody = null;
 
     private void Awake()
     {
@@ -26,6 +28,7 @@ public class Weapon : MonoBehaviour
     private void OnDisable()
     {
         interactable.onActivate.RemoveListener(Fire);
+        rbody = GetComponent<Rigidbody>();
 
     }
 
@@ -34,6 +37,9 @@ public class Weapon : MonoBehaviour
         var projectileObject = Instantiate(projectilePrefab, barrel.position, barrel.rotation);
         var projective = projectileObject.GetComponent<Projectile>();
         projective.Launch();
+
+
+        rbody.AddRelativeForce(Vector3.right * recoil, ForceMode.Impulse);
 
     }
 }
